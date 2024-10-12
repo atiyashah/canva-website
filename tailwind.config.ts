5fa796a6da851f8ee24b2c9a1b61321d5464c2f8
Syeda Atiya Shah <165414932+atiyashah@users.noreply.github.com>
2024-10-13 04:38:37 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        sans: ['var(--font-inknut-antiqua)']
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        shwhite:"#F8FAE5",
+        shblack:"#202020",
+        shbrown:"#76453B",
+        shorange:"#B19470",
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
